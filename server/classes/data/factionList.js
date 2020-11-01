5901bf4cb2f3b9cf9082de2e52261bb062a8e4e6
Jeremy Kalgreen <jeremy@jeremykalgreen.com>
2020-11-01 13:25:35 -0500

    Change ninjas status and reword guerillas sheet

--- server/classes/data/factionList.js
+++ server/classes/data/factionList.js
@@ -12,7 +12,7 @@ let obj = {
     vampires : { name: 'vampires', owner : null, killer : true, status : 2 },
     society : { name: 'society', owner : null, status : 1 },
     hackers : { name: 'hackers', owner : null, status : 1 },
-    ninjas : { name: 'ninjas', owner : null, killer : true, status : 0 },
+    ninjas : { name: 'ninjas', owner : null, killer : true, status : 1 },
     guerrillas : { name: 'guerrillas', owner : null, killer : true, status : 1 },
     parasites : { name: 'parasites', owner : null, status : 0, unselectable : true },
     conquistadors : { name: 'conquistadors', owner : null, status : 2 },
