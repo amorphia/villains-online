242211209a3ba6a61f53c371e4d156844b2febf7
Jeremy Kalgreen <jeremy@jeremykalgreen.com>
2024-05-16 18:21:33 -0400

    fix bug again

--- server/classes/planTests/controlAreasAndTest.js
+++ server/classes/planTests/controlAreasAndTest.js
@@ -8,7 +8,8 @@
  */
 const test = function controlAreasAnd( debug, faction, areaNames ) {
     let result = areaNames.every( area => {
-        let areaController = faction.game().areas[ area ].data?.owner;
+        let areaObject = faction.game().areas[ area ];
+        let areaController = areaObject ? areaObject.data?.owner : null;
         return areaController === faction.name;
     });
 
