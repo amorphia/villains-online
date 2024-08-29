c85192c81108e7eee140abc643c31d80147a91d6
Jeremy Kalgreen <jeremy@jeremykalgreen.com>
2024-08-29 19:51:14 -0400

    fix bug

--- server/classes/factions/plans/clonersPlans.js
+++ server/classes/factions/plans/clonersPlans.js
@@ -141,7 +141,7 @@ let obj = [
             },
             {
                 requirements : [
-                    { test : 'useSkills', args : [3] },
+                    { test : 'useSkills', args : [2] },
                 ],
                 value : 1
             },
