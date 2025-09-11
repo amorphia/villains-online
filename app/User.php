2c116b4845371fc6911baff53d8440c88df6f30b
Jeremy Kalgreen <jeremy@jeremykalgreen.com>
2025-09-10 23:14:18 -0400

    add more saved games

--- app/User.php
+++ app/User.php
@@ -91,7 +91,7 @@ class User extends Authenticatable
         return $this->games()
             ->with(['players'])
             ->has('saves')
-            ->take( 2 )
+            ->take( 6 )
             ->orderByDesc( 'created_at' )
             ->get();
     }
